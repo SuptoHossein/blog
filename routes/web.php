7d2bf81cb202d80390d3530056e79c7a624f13c3
suptohossein <supto.hossein@gmail.com>
2021-08-21 20:48:16 +0600

    setup frontend and backend

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// admin routes
+Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function() {
+    Route::get('/dashboard', function() {
+        return view('admin.dashboard.index');
+    });
+
+    Route::resource('category', 'CategoryController');
+});
+
+
+Route::get('/', function() {
+    return view('website.home');
+});
+
+Route::get('/about', function() {
+    return view('website.about');
+});
+
+Route::get('/category', function() {
+    return view('website.category');
+});
+
+Route::get('/contact', function() {
+    return view('website.contact');
+});
+
+Route::get('/post', function() {
+    return view('website.post');
+});
