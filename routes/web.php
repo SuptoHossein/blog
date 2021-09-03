4d6e599ea1f92e9b46fb735d47d7f198647cd537
suptohossein <supto.hossein@gmail.com>
2021-09-03 22:43:54 +0600

    frontendcontroller add, recent posts show on home page, route change

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+// frontend routes
+Route::get('/', 'FrontEndController@home')->name('website');
+Route::get('/about', 'FrontEndController@about')->name('website.about');
+Route::get('/category', 'FrontEndController@category')->name('website.category');
+Route::get('/contact', 'FrontEndController@contact')->name('website.contact');
+Route::get('/post/{slug}', 'FrontEndController@post')->name('website.post');
+
 
 // admin routes
 Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function() {
@@ -18,25 +25,4 @@ Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function() {
     Route::resource('category', 'CategoryController');
     Route::resource('tag', 'TagController');
     Route::resource('post', 'PostController');
-});
-
-
-Route::get('/', function() {
-    return view('website.home');
-})->name('website');
-
-Route::get('/about', function() {
-    return view('website.about');
-});
-
-Route::get('/category', function() {
-    return view('website.category');
-});
-
-Route::get('/contact', function() {
-    return view('website.contact');
-});
-
-Route::get('/post', function() {
-    return view('website.post');
 });
