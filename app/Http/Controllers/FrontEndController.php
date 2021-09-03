16c01109fc47ce3849bb501065e8a1f73a8e49bb
suptohossein <supto.hossein@gmail.com>
2021-09-03 23:49:46 +0600

    complete home page of frontend

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -9,10 +9,21 @@ class FrontEndController extends Controller
 {
     public function home()
     {
-        $posts = Post::orderBy('created_at', 'DESC')->take(5)->get();
-        $recentPosts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
+        $posts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->take(5)->get();
 
-        return view('website.home', compact(['posts', 'recentPosts']));
+        $firstPosts2 = $posts->splice(0,2);
+        $middlePost1 = $posts->splice(0,1);
+        $lastPosts = $posts->splice(0,2);
+
+
+        $footerPosts = $posts = Post::with('category', 'user')->inRandomOrder()->limit(4)->get();
+
+        $firstFooterPost = $posts->splice(0,1);
+        $firstFooterPost2 = $posts->splice(0,2);
+        $lastFooterPost = $posts->splice(0,1);
+
+        $recentPosts = Post::with('category', 'user')->orderBy('created_at', 'DESC')->paginate(9);
+        return view('website.home', compact(['posts', 'recentPosts', 'firstPosts2', 'middlePost1', 'lastPosts', 'firstFooterPost', 'firstFooterPost2', 'lastFooterPost']));
     }
 
     public function about()
