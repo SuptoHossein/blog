5937636b8f01cdca6f4a3751b2383be3264b0ca5
suptohossein <supto.hossein@gmail.com>
2021-12-14 19:48:35 +0600

    redesing blog description on frontend view

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -81,7 +81,7 @@
                             <span>&nbsp;-&nbsp; {{ $post->created_at->format('M d, Y') }}</span>
                         </div>
 
-                        <p>{{ Str::limit($post->description, 100, '...') }}</p>
+                        <p>{! Str::limit($post->description, 100, '...') !}</p>
                         <p><a href="{{ route('website.post', ['slug' => $post->slug]) }}">Read More</a></p>
                     </div>
                 </div>
